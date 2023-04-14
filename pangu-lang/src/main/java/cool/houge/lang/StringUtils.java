bd907fb4cb695d7d18e2e7b50b53a7ecf715e49e
ZY <kzou227@qq.com>
2023-04-14 12:06:08 +0800

    添加String工具类

---
+++ pangu-lang/src/main/java/cool/houge/lang/StringUtils.java
@@ -0,0 +1,27 @@
+package cool.houge.lang;
+
+/**
+ * 字符串工具类.
+ *
+ * @author ZY (kzou227@qq.com)
+ */
+public final class StringUtils {
+
+    /**
+     * 字符串是否为<code>null</code>或空字符串.
+     * @param s 字符串
+     * @return true/false
+     */
+    public static boolean isEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    /**
+     * 字符串是否非<code>null</code>或空字符串.
+     * @param s 字符串
+     * @return true/false
+     */
+    public static boolean isNotEmpty(String s) {
+        return !isEmpty(s);
+    }
+}

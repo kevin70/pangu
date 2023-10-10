aa4ab0213dae5763f1c205b8e4f866da121c9be0
ZY <kzou227@qq.com>
2023-10-10 18:53:59 +0800

    增加 helidon jsonb 序列化

---
+++ pangu-lang/src/main/java/cool/houge/helidon/http/media/JsonbSupport.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cool.houge.helidon.http.media;
+
+import io.avaje.jsonb.Jsonb;
+import io.helidon.common.GenericType;
+import io.helidon.common.config.Config;
+import io.helidon.common.media.type.MediaTypes;
+import io.helidon.http.*;
+import io.helidon.http.media.EntityReader;
+import io.helidon.http.media.EntityWriter;
+import io.helidon.http.media.MediaSupport;
+
+import java.util.Objects;
+
+import static io.helidon.http.HeaderValues.CONTENT_TYPE_JSON;
+
+/**
+ * {@link java.util.ServiceLoader} provider implementation for JSON Binding media support.
+ */
+public class JsonbSupport implements MediaSupport {
+
+    private static final Jsonb JSON_B = Jsonb.builder().build();
+
+    private final JsonbReader reader = new JsonbReader(JSON_B);
+    private final JsonbWriter writer = new JsonbWriter(JSON_B);
+
+    private final String name;
+
+    private JsonbSupport(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Creates a new {@link JsonbSupport}.
+     *
+     * @param config must not be {@code null}
+     * @return a new {@link JsonbSupport}
+     */
+    public static MediaSupport create(Config config) {
+        return create(config, "avaje-jsonb");
+    }
+
+    /**
+     * Creates a new {@link JsonbSupport}.
+     *
+     * @param config must not be {@code null}
+     * @param name name of this instance
+     * @return a new {@link JsonbSupport}
+     * @see #create(Config)
+     */
+    public static MediaSupport create(Config config, String name) {
+        Objects.requireNonNull(config);
+        Objects.requireNonNull(name);
+
+        return new JsonbSupport(name);
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String type() {
+        return "avaje-jsonb";
+    }
+
+    @Override
+    public <T> ReaderResponse<T> reader(GenericType<T> type, Headers requestHeaders) {
+        if (requestHeaders
+                .contentType()
+                .map(it -> it.test(MediaTypes.APPLICATION_JSON))
+                .orElse(true)) {
+            return new ReaderResponse<>(SupportLevel.COMPATIBLE, this::reader);
+        }
+
+        return ReaderResponse.unsupported();
+    }
+
+    @Override
+    public <T> WriterResponse<T> writer(
+            GenericType<T> type, Headers requestHeaders, WritableHeaders<?> responseHeaders) {
+        // check if accepted
+        for (HttpMediaType acceptedType : requestHeaders.acceptedTypes()) {
+            if (acceptedType.test(MediaTypes.APPLICATION_JSON)) {
+                return new WriterResponse<>(SupportLevel.COMPATIBLE, this::writer);
+            }
+        }
+
+        if (requestHeaders.acceptedTypes().isEmpty()) {
+            return new WriterResponse<>(SupportLevel.COMPATIBLE, this::writer);
+        }
+
+        return WriterResponse.unsupported();
+    }
+
+    @Override
+    public <T> ReaderResponse<T> reader(GenericType<T> type, Headers requestHeaders, Headers responseHeaders) {
+        // check if accepted
+        for (HttpMediaType acceptedType : requestHeaders.acceptedTypes()) {
+            if (acceptedType.test(MediaTypes.APPLICATION_JSON)
+                    || acceptedType.mediaType().isWildcardType()) {
+                return new ReaderResponse<>(SupportLevel.COMPATIBLE, this::reader);
+            }
+        }
+
+        if (requestHeaders.acceptedTypes().isEmpty()) {
+            return new ReaderResponse<>(SupportLevel.COMPATIBLE, this::reader);
+        }
+
+        return ReaderResponse.unsupported();
+    }
+
+    @Override
+    public <T> WriterResponse<T> writer(GenericType<T> type, WritableHeaders<?> requestHeaders) {
+        if (requestHeaders.contains(HeaderNames.CONTENT_TYPE)) {
+            if (requestHeaders.contains(CONTENT_TYPE_JSON)) {
+                return new WriterResponse<>(SupportLevel.COMPATIBLE, this::writer);
+            }
+        } else {
+            return new WriterResponse<>(SupportLevel.SUPPORTED, this::writer);
+        }
+        return WriterResponse.unsupported();
+    }
+
+    <T> EntityReader<T> reader() {
+        return reader;
+    }
+
+    <T> EntityWriter<T> writer() {
+        return writer;
+    }
+}

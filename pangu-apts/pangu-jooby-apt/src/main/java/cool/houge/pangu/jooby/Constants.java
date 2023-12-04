9eb0855740af9ab992701dd2becbeab56e5ea513
kk <kzou227@qq.com>
2023-12-04 20:50:56 +0800

    删除 APT 项目

--- pangu-apts/pangu-jooby-apt/src/main/java/cool/houge/pangu/jooby/Constants.java
+++
@@ -1,9 +0,0 @@
-package cool.houge.pangu.jooby;
-
-/**
- * @author ZY (kzou227@qq.com)
- */
-public final class Constants {
-
-    public static final String PATH = "io.jooby.annotation.Path";
-}
--- pangu-apts/pangu-jooby-apt/src/test/resources/OpenAPIDefinitionApplication.java
+++
@@ -1,5 +0,0 @@
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
-@OpenAPIDefinition(info = @Info(title = "OpenAPIDefinitionApplication", description = "这是测试文档", version = "1.0.0"))
-public class OpenAPIDefinitionApplication {}
